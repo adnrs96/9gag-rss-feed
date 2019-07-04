7b5760ac6557ca14aebeab36dfdd14f6370c1f9e
Aditya Bansal <adi.bansal241996@gmail.com>
2019-07-05 03:00:54 +0530

    feed.html: Add code to render feed templates and cache them.

--- flaskr/server.py
+++ flaskr/server.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
+from werkzeug.contrib.cache import SimpleCache
+from flask import Flask, render_template, abort
 from flaskr.feed_list import feeds
+from flaskr.fetch import fetch_feed_and_parse
+import logging
+
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+rss_template_cache = SimpleCache()
+
+def get_rendered_template_for_feed(feed_id):
+    template = rss_template_cache.get(feed_id)
+    if template is None:
+        data = fetch_feed_and_parse(feeds[feed_id]['rss_link'])
+        if data is None:
+            logging.error('Cannot render template for feed', feed_id)
+            return abort(500)
+
+        feed_ctx = {
+            "name": data.feed.title,
+            "link": data.feed.link,
+            "items": data.entries,
+        }
+        template = render_template("feed.html", **feed_ctx)
+        rss_template_cache.set(feed_id, template, timeout=2*60)
+    return template
 
 @app.route('/', methods=['GET'])
 def index():
