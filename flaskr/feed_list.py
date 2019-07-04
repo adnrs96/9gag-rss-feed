1821d7f1bc45ccccbe44bbe1ebc4993ba6d41886
Aditya Bansal <adi.bansal241996@gmail.com>
2019-07-05 02:12:23 +0530

    flaskr: Move out feeds data from main server code.

---
+++ flaskr/feed_list.py
@@ -0,0 +1,18 @@
+feeds = {
+    "1" : {
+        "name": "9GAG - Awesome",
+        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGAwesome&format=2",
+    },
+    "2": {
+        "name": "9GAG - Comic",
+        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGComic&format=2",
+    },
+    "3": {
+        "name": "9GAG - Dark Humor",
+        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGDarkHumor&format=2",
+    },
+    "4": {
+        "name": "9GAG - Fresh",
+        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGFresh&format=2",
+    },
+}
--- flaskr/server.py
+++ flaskr/server.py
@@ -1,31 +1,9 @@
 from flask import Flask, render_template
+from flaskr.feed_list import feeds
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-feeds = [
-    {
-        "name": "9GAG - Awesome",
-        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGAwesome&format=2",
-        "link": "/feed/1",
-    },
-    {
-        "name": "9GAG - Comic",
-        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGComic&format=2",
-        "link": "/feed/2",
-    },
-    {
-        "name": "9GAG - Dark Humor",
-        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGDarkHumor&format=2",
-        "link": "/feed/3",
-    },
-    {
-        "name": "9GAG - Fresh",
-        "rss_link": "https://9gag-rss.com/api/rss/get?code=9GAGFresh&format=2",
-        "link": "/feed/4",
-    },
-
-]
-
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', feeds=feeds)
+    index_ctx = [{"name": feed["name"], "link": "/feed/"+str(id)} for id, feed in feeds.items()]
+    return render_template('index.html', feeds=index_ctx)
